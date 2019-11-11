ecfabf2bc39052d445fc031b2bc83e0fdf870d49
ryanyogan <ryanyogan@gmail.com>
2019-11-11 13:49:37 -0800

    Edit an delete

--- lib/poller_phx_web/controllers/choice_controller.ex
+++ lib/poller_phx_web/controllers/choice_controller.ex
@@ -56,4 +56,48 @@ defmodule PollerPhxWeb.ChoiceController do
         )
     end
   end
+
+  def edit(conn, %{"id" => id, "district_id" => district_id, "question_id" => question_id}) do
+    choice = Choices.get_choice!(id)
+    changeset = Choices.change_choice(choice)
+    questions = Questions.list_questions()
+    district = Districts.get_district!(district_id)
+    question = Questions.get_question!(question_id)
+
+    render(conn, "edit.html",
+      choice: choice,
+      changeset: changeset,
+      questions: questions,
+      district: district,
+      question: question
+    )
+  end
+
+  def update(conn, %{
+        "id" => id,
+        "choice" => choice_params,
+        "district_id" => district_id,
+        "question_id" => question_id
+      }) do
+    choice = Choices.get_choice!(id)
+
+    case Choices.update_choice(choice, choice_params) do
+      {:ok, _choice} ->
+        conn
+        |> put_flash(:info, "Choice updated successfully.")
+        |> redirect(to: Routes.choice_path(conn, :index, district_id, question_id))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", choice: choice, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id, "district_id" => district_id, "question_id" => question_id}) do
+    choice = Choices.get_choice!(id)
+    {:ok, _choice} = Choices.delete_choice(choice)
+
+    conn
+    |> put_flash(:info, "Choice deleted successfully.")
+    |> redirect(to: Routes.choice_path(conn, :index, district_id, question_id))
+  end
 end
